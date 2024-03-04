c329fe2f743a717ec9b2572feea839b4e34a6eb7
Gustavo Fernandes de Moraes <pcvgustavo@gmail.com>
2024-03-04 16:58:45 -0300

    some changes im my personal informations

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request
 import requests
+from requirements import API_KEY
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def index():
+def index() -> None:
     return render_template('index.html')
 
 @app.route("/weather")
-def weather():
+def weather() -> None:
+        
     city = request.args.get('city')
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=c32cd803940fee998e5be7a343aa5a4b&units=metric'
-    r = requests.get(url.format(city)).json
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
     response = requests.get(url)
     data = response.json()
     weather_info = {
